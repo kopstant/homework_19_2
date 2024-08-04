801e748c23cbdf5a26f8fd5ebe5b0dfe8dc54d24
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2024-08-04 14:33:55 +0900

    Renaming app_name from "webstore" to catalog. Make first app "catalog" and and the settings are added

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from webshop import views
+from catalog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('webshop.urls', namespace='webshop')),
+    path('', include('catalog.urls', namespace='catalog')),
 ]
